c4530c3ad0f1533bebaaa1c4b19690d7071de7a1
Soumen Khara <soumen2015.s.k@gmail.com>
2020-12-28 20:51:45 +0530

    make it better

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,13 +1,1 @@
-const socket = io("/");
-const name = prompt("Enter your name");
-socket.on("connect", () => {
-  console.log("socket connected");
-});
 
-const send = (data) => {
-  socket.emit("client-send", data);
-};
-
-socket.on("client-podcast", (data) => {
-  console.log(data);
-});
--- server.js
+++ server.js
@@ -2,14 +2,55 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+var users = {};
 
 app.use(express.static("public"));
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.render("index.ejs", { roomId: req.query.room });
+});
+const checkName = (req, res, next) => {
+  if (req.query.name == undefined) {
+    return res.redirect(`/?room=${req.params.id}`);
+  }
+  return next();
+};
+app.post("/create-room", (req, res) => {
+  res.redirect(`/${uuidV4()}?name=${req.body.name}`);
+});
+
+app.get("/:id", checkName, (req, res) => {
+  res.render("chat.ejs", { roomId: req.params.id, name: req.query.name });
+});
+
+app.post("/join-room", (req, res) => {
+  res.redirect(`/${req.body.room_id}?name=${req.body.name}`);
+});
 
 io.on("connection", (socket) => {
   // console.log(Object.keys(io.sockets.server.engine.clients), socket.id);
   console.log("socket connected");
-  socket.on("client-send", (data) => {
-    io.emit("client-podcast", data);
+  socket.on("join-room", (roomId, name) => {
+    users[`${socket.id}`] = name;
+
+    socket.on("disconnecting", () => {
+      socket.to(roomId).emit("user-disconnected", users[socket.id]);
+      delete users[`${socket.id}`];
+      console.log("disconnecting: " + socket.id);
+    });
+
+    socket.to(roomId).emit("user-connect", name);
+
+    console.log("Room join");
+
+    socket.join(roomId);
+
+    socket.on("client-send", (data, name) => {
+      socket.to(roomId).emit("client-podcast", data, name);
+    });
   });
 });
 
